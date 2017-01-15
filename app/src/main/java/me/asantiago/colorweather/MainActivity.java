a40eb9f8f0af3e2213530d21f8876f9dab48047c
Amador368 <amadorsantiago368@gmail.com>
2017-01-14 22:47:14 -0600

    Adding intent for daily weather

--- app/src/main/java/me/asantiago/colorweather/MainActivity.java
+++ app/src/main/java/me/asantiago/colorweather/MainActivity.java
@@ -1,13 +1,35 @@
 package me.asantiago.colorweather;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView dailyWeatherTextView;
+    private TextView hourlyWeatherTextView;
+    private TextView minutelyWeatherTextView;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dailyWeatherTextView = (TextView) findViewById(R.id.dailyBtnTextView);
+        hourlyWeatherTextView = (TextView) findViewById(R.id.hourlyBtnTextView);
+        minutelyWeatherTextView = (TextView) findViewById(R.id.minutelyBtnTextView);
+
+        dailyWeatherTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.d(TAG,"mensaje click en dailly text btn");
+                Intent dailyActivityIntent = new Intent(MainActivity.this, DailyWeatherActivity.class);
+                startActivity(dailyActivityIntent);
+            }
+        });
+
     }
 }
