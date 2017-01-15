c5de915ec4c13fe4bda18cf695368863436ea81d
Amador368 <amadorsantiago368@gmail.com>
2017-01-15 10:33:18 -0600

    Creating Current Weather class for model

--- app/src/main/java/me/asantiago/colorweather/MainActivity.java
+++ app/src/main/java/me/asantiago/colorweather/MainActivity.java
@@ -2,7 +2,14 @@ package me.asantiago.colorweather;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import butterknife.BindDrawable;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -10,11 +17,40 @@ public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.iconWeatherImageView) ImageView iconWeatherImageView;
+    @BindView(R.id.iconDescriptionTextView) TextView iconDescriptionTextView;
+    @BindView(R.id.currentTempTextView) TextView currentTempTextView;
+    @BindView(R.id.highestTempTextView) TextView highestTempTextView;
+    @BindView(R.id.lowestTempTextView) TextView lowestTempTextView;
+
+    @BindDrawable(R.drawable.clear_night)
+    Drawable clearNight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        //Using CurrentWeather class
+        CurrentWeather currentWeather = new CurrentWeather(MainActivity.this);
+
+        currentWeather.setIconImage("rain");
+        currentWeather.setDescription("Raining");
+        currentWeather.setCurrentTemperature("26");
+        currentWeather.setHighestTemperature("28");
+        currentWeather.setLowestTemperature("10");
+
+        //init values
+        iconWeatherImageView.setImageDrawable(currentWeather.getIconDrawableResource());
+        iconDescriptionTextView.setText(currentWeather.getDescription());
+        lowestTempTextView.setText(currentWeather.getLowestTemperature());
+        highestTempTextView.setText(currentWeather.getHighestTemperature());
+        currentTempTextView.setText(currentWeather.getCurrentTemperature());
+
+
+
+
     }
     //Bind with Butterknife
     @OnClick(R.id.dailyBtnTextView)
