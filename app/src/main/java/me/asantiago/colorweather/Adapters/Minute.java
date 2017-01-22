753996bc6e9ad4dbc034a944f112ae0fa418b601
Amador368 <amadorsantiago368@gmail.com>
2017-01-21 19:14:15 -0600

    Creating layout for minutely weather with recycler view

---
+++ app/src/main/java/me/asantiago/colorweather/Adapters/Minute.java
@@ -0,0 +1,29 @@
+package me.asantiago.colorweather.Adapters;
+
+/**
+ * Created by asantiago on 21/01/17.
+ */
+
+public class Minute {
+    private String title;
+    private String rainProbability;
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+}
--- app/src/main/java/me/asantiago/colorweather/MinutelyWeatherActivity.java
+++ app/src/main/java/me/asantiago/colorweather/MinutelyWeatherActivity.java
@@ -2,12 +2,45 @@ package me.asantiago.colorweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.asantiago.colorweather.Adapters.Minute;
+import me.asantiago.colorweather.Adapters.MinutelyWeatherAdapter;
 
 public class MinutelyWeatherActivity extends Activity {
+
+    @BindView(R.id.MinutelyRecyclerView)
+    RecyclerView minutelyRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minutely_weather);
+
+        ButterKnife.bind(this);
+
+        ArrayList<Minute> minutes = new ArrayList<>();
+
+        Minute minute = new Minute();
+
+        minute.setTitle("12:23");
+        minute.setRainProbability("34%");
+
+        minutes.add(minute);
+
+        MinutelyWeatherAdapter minutelyWeatherAdapter = new MinutelyWeatherAdapter(this, minutes);
+
+        minutelyRecyclerView.setAdapter(minutelyWeatherAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        minutelyRecyclerView.setLayoutManager(layoutManager);
+        minutelyRecyclerView.setHasFixedSize(true);
+
     }
 }
